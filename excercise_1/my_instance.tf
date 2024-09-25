1729f865f83b666f03750b0e29cafa7e2054cfd2
SajanaRajapaksha <sajanavol@gmail.com>
2024-09-25 17:05:01 +0530

    ex1

--- excercise_1/my_instance.tf
+++ excercise_1/my_instance.tf
@@ -1,14 +1,15 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "intro" {
-    ami = "ami-0ebfd941bbafe70c6"
-    instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
-    key_name = "terra-key"
-    vpc_security_group_ids = ["sg-0d6b2d6b4b3b43319"]
-    tags = {
-      Name = "terra-instance"
-    }
+  ami                    = "ami-0ebfd941bbafe70c6"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1a"
+  key_name               = "terra-key"
+  vpc_security_group_ids = ["sg-0d6b2d6b4b3b43319"]
+  tags = {
+    Name    = "terra-instance"
+    Project = "terra-learn"
+  }
 }
