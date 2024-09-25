0269dc37998fc3d5cda4f79ec7a609b27a062031
SajanaRajapaksha <sajanavol@gmail.com>
2024-09-25 20:32:01 +0530

    ex4-all

---
+++ excercise_4/instance.tf
@@ -0,0 +1,46 @@
+resource "aws_key_pair" "terra-key-3-aws" {
+  key_name   = "terra-key-3-aws"
+  public_key = file("terra-key-3.pub")
+}
+
+resource "aws_instance" "terra-inst-3" {
+  ami                    = var.AMIS[var.REGION]
+  instance_type          = "t2.micro"
+  availability_zone      = var.ZONE1
+  key_name               = aws_key_pair.terra-key-3-aws.key_name
+  vpc_security_group_ids = ["sg-0d6b2d6b4b3b43319"]
+  tags = {
+    Name    = "terra-inst-3"
+    Project = "terra-excercise-3"
+  }
+
+  //pass the file to instance
+  provisioner "file" {
+    source      = "web.sh"
+    destination = "/tmp/web.sh"
+  }
+
+  //connect or login to the instance
+  connection {
+    user        = var.USER
+    private_key = file("terra-key-3")
+    host        = self.public_ip //use public ip of the instance to login
+  }
+
+  //remote execution of web.sh
+  provisioner "remote-exec" {
+    inline = [
+      "chmod u+x /tmp/web.sh",
+      "sudo /tmp/web.sh"
+    ]
+  }
+}
+
+//output
+output "public_ip" {
+  value = aws_instance.terra-inst-3.public_ip
+}
+output "private_ip" {
+  value = aws_instance.terra-inst-3.private_ip
+}
+
